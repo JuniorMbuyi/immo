624c4410c70b6f12742895c5966e0e360268dee9
JuniorMbuyi <Juiormbuyi4000@gmail.com>
2022-10-01 23:34:43 +0100

    modification compteur

--- souzy/settings.py
+++ souzy/settings.py
@@ -29,7 +29,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 else :
     DEBUG = True
 
-ALLOWED_HOSTS = ['immosouza.herokuapp.com','www.souzaimmo.com']
+ALLOWED_HOSTS = ['immosouza.herokuapp.com','www.souzaimmo.com','127.0.0.1']
 
 
 # Application definition
